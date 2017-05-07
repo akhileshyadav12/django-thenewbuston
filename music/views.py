57065a796c074cfa6519e59a50385ac4a440bd69
hadi <me@hadikhan.com>
2017-05-07 18:23:24 +0500

    Lecture 16

--- music/views.py
+++ music/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Albums
 
 # Create your views here.
 
 def index(request):
     all_albums = Albums.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def details(request, album_id):
-    return HttpResponse('<h2>Details for Album ID: ' + str(album_id) + '</h2>')
+    try:
+        album = Albums.objects.get(pk=album_id)
+    except Albums.DoesNotExist:
+        raise Http404('Album Doesnot exit')
+    return render(request, 'music/detail.html', {'album':album})
 
