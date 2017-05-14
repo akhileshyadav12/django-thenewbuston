2f6fb790d3a95df7948685b3501ec25a1da177d5
hadi <me@hadikhan.com>
2017-05-15 00:12:01 +0500

    Lecture 24

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,11 @@ app_name = 'music'
 urlpatterns = [
     # /music
     url(r'^$', views.index, name='index'),
+
     #/music/75
     url(r'^(?P<album_id>[0-9]+)/$', views.details, name='details'),
+
+    #/music/75/favourite
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Albums
+from .models import Albums, Songs
+import pdb
+
 
 # Create your views here.
 
@@ -10,4 +12,20 @@ def index(request):
 def details(request, album_id):
     album = get_object_or_404(Albums, pk=album_id)
     return render(request, 'music/detail.html', {'album':album})
+
+def favourite(request, album_id):
+    #print(album_id)
+    album = get_object_or_404(Albums, pk=album_id)
+    #request = request.POST['song']
+    try:
+        selected_song = album.songs_set.get(pk=request.POST['song'])
+    except (KeyError, Songs.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album':album,
+            'error_message':'You did not select a valid song',
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
 
